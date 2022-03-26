5cfd0180c8ff4e8cc708645ab41940c72a827339
riroan <riroan@naver.com>
2022-03-26 20:43:58 +0900

    컬러값 추가

---
+++ frontend/src/components/_config/color.tsx
@@ -0,0 +1,46 @@
+const color = {
+    unrated: '#2d2d2d',
+    bronze: '#ad5600',
+    silver: '#435f7a',
+    gold: '#ec9a00',
+    platinum: '#27e2a4',
+    diamond: '#00b4fc',
+    ruby: '#ff0062',
+}
+
+const tier2color = (tier: number, verbose: boolean = false) => {
+    if (verbose) {
+        if (tier >= 1 && tier <= 5) {
+            return 'bronze'
+        } else if (tier >= 6 && tier <= 10) {
+            return 'silver'
+        } else if (tier >= 11 && tier <= 15) {
+            return 'gold'
+        } else if (tier >= 16 && tier <= 20) {
+            return 'platinum'
+        } else if (tier >= 21 && tier <= 25) {
+            return 'diamond'
+        } else if (tier >= 26 && tier <= 30) {
+            return 'ruby'
+        }
+    } else {
+        switch (tier) {
+            case 1:
+                return 'bronze'
+            case 2:
+                return 'silver'
+            case 3:
+                return 'gold'
+            case 4:
+                return 'platinum'
+            case 5:
+                return 'diamond'
+            case 6:
+                return 'ruby'
+        }
+    }
+    return 'unrated'
+}
+
+export default color
+export { tier2color }
