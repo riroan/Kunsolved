b3da2b70bacf574040ca75c915bd36fe696d6413
riroan <riroan@naver.com>
2022-03-31 04:03:31 +0900

    메인페이지 요소 추가

---
+++ frontend/src/components/pages/MainPage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navigation from '../../navigation'
+import Menu from '../../menu'
+
+export default function MainPage() {
+    return (
+        <div>
+            <Menu />
+            <Navigation />
+        </div>
+    )
+}
