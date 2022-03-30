30f6c6429969674ec5413920dcc7de978f9fcc06
riroan <riroan@naver.com>
2022-03-31 04:03:08 +0900

    react-router-dom 패키지 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import Navigation from './components/navigation'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import MainPage from './components/pages/MainPage'
 
 function App() {
     return (
-        <div>
-            <Navigation />
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+            </Routes>
+        </Router>
     )
 }
 
