89024494b35746b387b1dbf72ed6e7d47eef83bc
riroan <riroan@naver.com>
2022-04-08 15:49:24 +0900

    lgtm 수정

--- frontend/src/components/issue/index.tsx
+++ frontend/src/components/issue/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import classnames from 'classnames/bind'
 import styles from './issue.module.scss'
 import URL from '../_config/config'
