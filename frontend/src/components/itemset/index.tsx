4c6ba8074c8f9cd905baa5526333d53eda407286
riroan <riroan@naver.com>
2022-04-10 19:55:52 +0900

    페이지네이션 구현

--- frontend/src/components/itemset/index.tsx
+++ frontend/src/components/itemset/index.tsx
@@ -1,18 +1,28 @@
-import React, { ReactElement } from 'react'
+import React, { useState, ReactElement } from 'react'
 import styles from './itemset.module.scss'
 import classnames from 'classnames/bind'
+import Pagination from '../pagination'
 const cx = classnames.bind(styles)
 
 type ItemSetProps = {
-    data?: ReactElement[]
+    data: ReactElement[]
 }
 
 export default function ItemSet({ data }: ItemSetProps) {
+    const title = data[0]
+    const d = data.slice(1)
+    const [limit, setLimit] = useState(20)
+    const [page, setPage] = useState(1)
+    const offset = (page - 1) * limit
     return (
         <div className={cx('main')}>
-            <ul className={cx('ul')}>
-                {data}
-            </ul>
+            <div className={ cx('item')}>
+                <ul className={cx('ul')}>
+                    {title}
+                    {d.slice(offset, offset + limit)}
+                </ul>
+            </div>
+            <Pagination total={d.length} limit={limit} page={page} setPage={setPage} />
         </div>
     )
 }
---
+++ frontend/src/components/pagination/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import styles from './pagination.module.scss'
+import classnames from 'classnames/bind'
+const cx = classnames.bind(styles)
+
+type PaginationProps = {
+    total: number
+    limit: number
+    page: number
+    setPage: (arg0: number) => void
+}
+
+export default function Pagination({ total, limit, page, setPage }: PaginationProps) {
+    const numPages = Math.ceil(total / limit)
+    var left = Math.max(page - 5, 1),
+        right = 10
+    if (numPages <= 10) {
+        left = 1
+        right = numPages
+    } else {
+        right = left === 1 ? Math.min(numPages, 10) : Math.min(page + 5, numPages)
+    }
+
+    return (
+        <div className={cx('main')}>
+            <button className={cx('button')} onClick={() => setPage(page - 1)} disabled={page === 1}>
+                &lt;
+            </button>
+            {Array(right - left + 1)
+                .fill(1)
+                .map((_, i) => (
+                    <button className={cx('button', left + i === page && 'current')} key={i + 1} onClick={() => setPage(left + i)}>
+                        {left + i}
+                    </button>
+                ))}
+            <button className={cx('button')} onClick={() => setPage(page + 1)} disabled={page === numPages}>
+                &gt;
+            </button>
+        </div>
+    )
+}
