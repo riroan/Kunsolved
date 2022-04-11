5e4b2052019655e8d3a00033618650e572c46314
riroan <riroan@naver.com>
2022-04-11 22:44:19 +0900

    주석 제거

--- frontend/src/components/weeklybest/index.tsx
+++ frontend/src/components/weeklybest/index.tsx
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import classnames from 'classnames/bind'
 import styles from './weeklybest.module.scss'
-import { AiFillTrophy } from 'react-icons/ai'
 import URL from '../_config/config'
 const cx = classnames.bind(styles)
 
 export default function WeeklyBest() {
     const [data, setData] = useState([])
     const [contribData, setContribData] = useState([])
-    const [feat, setFeat] = useState(false)
 
     useEffect(() => {
         var url = `${URL}/weeklyBest`
@@ -42,33 +40,13 @@ export default function WeeklyBest() {
         <div className={cx('main')}>
             <div className={cx('title')}>명예의 전당</div>
             <div className={cx('desc')}>(기록은 매주 월요일 초기화됩니다.)</div>
-            <div className={ cx('buttonSet')}>
-                {/*<button*/}
-                {/*    className={cx(!feat && 'selected', 'button')}*/}
-                {/*    onClick={() => {*/}
-                {/*        setFeat(false)*/}
-                {/*    }}*/}
-                {/*>*/}
-                {/*    푼 문제*/}
-                {/*</button>*/}
-                {/*<button*/}
-                {/*    className={cx(feat && 'selected', 'button')}*/}
-                {/*    onClick={() => {*/}
-                {/*        setFeat(true)*/}
-                {/*    }}*/}
-                {/*>*/}
-                {/*    랭킹 기여*/}
-                {/*</button>*/}
-            </div>
             <div className={cx('rank-wrapper')}>
-
                 <div className={cx('solved-rank')}>
                     <div className={cx('rank-title')}>푼 문제</div>
                 {data.map((value, i) => {
                           return (
                               <a href={`https://www.acmicpc.net/user/${value['name']}`} className={cx('a')} key={i}>
                                   <div className={cx(i === 0 ? 'first' : i <= 3 ? 'second' : 'third', 'item')}>
-                                      {/*<AiFillTrophy className={cx('icon')} />*/}
                                       <div className={cx('rank')}>#{i + 1}</div>
                                       <div className={cx('name')}>{value['name']}</div>
                                       <div className={cx('cnt')}>{value['cnt']}</div>
@@ -84,7 +62,6 @@ export default function WeeklyBest() {
                         return (
                             <a href={`https://www.acmicpc.net/user/${value['name']}`} className={cx('a')} key={i}>
                                 <div className={cx(i === 0 ? 'first' : i <= 3 ? 'second' : 'third', 'item')}>
-                                    {/*<AiFillTrophy className={cx('icon')} />*/}
                                     <div className={cx('rank')}>#{i + 1}</div>
                                     <div className={cx('name')}>{value['name']}</div>
                                     <div className={cx('cnt')}>{value['cnt']}</div>
