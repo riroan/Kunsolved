b7e4fe8c25fb3045d5c00ca8c15209e261721ab0
donghoony <aru0504@naver.com>
2022-04-11 19:08:57 +0900

    Front: design rank table

--- frontend/src/components/weeklybest/index.tsx
+++ frontend/src/components/weeklybest/index.tsx
@@ -43,36 +43,58 @@ export default function WeeklyBest() {
             <div className={cx('title')}>명예의 전당</div>
             <div className={cx('desc')}>(기록은 매주 월요일 초기화됩니다.)</div>
             <div className={ cx('buttonSet')}>
-                <button
-                    className={cx(!feat && 'selected', 'button')}
-                    onClick={() => {
-                        setFeat(false)
-                    }}
-                >
-                    푼 문제
-                </button>
-                <button
-                    className={cx(feat && 'selected', 'button')}
-                    onClick={() => {
-                        setFeat(true)
-                    }}
-                >
-                    랭킹 기여
-                </button>
+                {/*<button*/}
+                {/*    className={cx(!feat && 'selected', 'button')}*/}
+                {/*    onClick={() => {*/}
+                {/*        setFeat(false)*/}
+                {/*    }}*/}
+                {/*>*/}
+                {/*    푼 문제*/}
+                {/*</button>*/}
+                {/*<button*/}
+                {/*    className={cx(feat && 'selected', 'button')}*/}
+                {/*    onClick={() => {*/}
+                {/*        setFeat(true)*/}
+                {/*    }}*/}
+                {/*>*/}
+                {/*    랭킹 기여*/}
+                {/*</button>*/}
             </div>
-            {(feat
-                ? contribData : data).map((value, i) => {
-                      return (
-                          <a href={`https://www.acmicpc.net/user/${value['name']}`} className={cx('a')} key={i}>
-                              <div className={cx(i === 0 ? 'first' : i <= 3 ? 'second' : 'third', 'item')}>
-                                  <AiFillTrophy className={cx('icon')} />
-                                  <div className={cx('rank')}>{i + 1}</div>
-                                  <div className={cx('name')}>{value['name']}</div>
-                                  <div className={cx('cnt')}>{value['cnt']} solved</div>
-                              </div>
-                          </a>
+            <div className={cx('rank-wrapper')}>
+
+                <div className={cx('solved-rank')}>
+                    <div className={cx('rank-title')}>푼 문제</div>
+                {data.map((value, i) => {
+                          return (
+                              <a href={`https://www.acmicpc.net/user/${value['name']}`} className={cx('a')} key={i}>
+                                  <div className={cx(i === 0 ? 'first' : i <= 3 ? 'second' : 'third', 'item')}>
+                                      {/*<AiFillTrophy className={cx('icon')} />*/}
+                                      <div className={cx('rank')}>#{i + 1}</div>
+                                      <div className={cx('name')}>{value['name']}</div>
+                                      <div className={cx('cnt')}>{value['cnt']}</div>
+                                  </div>
+                              </a>
                       )
-                  })}
+                  })
+                }
+                </div>
+                <div className={cx('distribution-rank')}>
+                    <div className={cx('rank-title')}>학교 랭킹 기여</div>
+                    {contribData.map((value, i) => {
+                        return (
+                            <a href={`https://www.acmicpc.net/user/${value['name']}`} className={cx('a')} key={i}>
+                                <div className={cx(i === 0 ? 'first' : i <= 3 ? 'second' : 'third', 'item')}>
+                                    {/*<AiFillTrophy className={cx('icon')} />*/}
+                                    <div className={cx('rank')}>#{i + 1}</div>
+                                    <div className={cx('name')}>{value['name']}</div>
+                                    <div className={cx('cnt')}>{value['cnt']}</div>
+                                </div>
+                            </a>
+                        )
+                    })
+                    }
+                </div>
+            </div>
         </div>
     )
 }
