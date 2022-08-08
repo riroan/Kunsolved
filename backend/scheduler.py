f59e0344c27c8664c42d0af812322ceb7b8f05d8
riroan <riroan@naver.com>
2022-08-09 02:30:27 +0900

    feat: scheduler 업데이트

--- backend/scheduler.py
+++ backend/scheduler.py
@@ -5,6 +5,9 @@ import time
 util = Utility(True)
 
 schedule.every().monday.at("05:00").do(util.updateAllUserSolved)
+schedule.every(5).minutes.do(util.getRecentSolved)
+schedule.every().day.at("03:00").do(util.getProblemInfo)
+schedule.every().day.at("02:00").do(util.updateSchoolUser)
 while True:
     schedule.run_pending()
     time.sleep(1)
