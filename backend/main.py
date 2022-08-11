ee19260a0cb5261619a5f48e62a1d698e17120a6
riroan <riroan@naver.com>
2022-08-12 00:20:04 +0900

    chore: 로그 추가

--- backend/main.py
+++ backend/main.py
@@ -30,6 +30,8 @@ app.add_middleware(
 
 @app.get("/v1/level")
 async def byLevel():
+    now = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+    print(f">> Log ({now}) : /v1/level")
     data = util.getCountSolvedByLevel()
     ret = dict()
     for i, v in enumerate(data):
@@ -40,6 +42,8 @@ async def byLevel():
 
 @app.get("/v1/exp")
 async def byExp():
+    now = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+    print(f">> Log ({now}) : /v1/exp")
     data = util.getCountSolvedByLevel(1)
     exp = util.getAllExp()
     ret = dict()
@@ -51,6 +55,8 @@ async def byExp():
 
 @app.get("/v1/tag")
 async def solvedByTag():
+    now = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+    print(f">> Log ({now}) : /v1/tag")
     d = {"수학": "math", "구현": "implementation", "그리디 알고리즘": "greedy", "문자열": "string", "자료 구조": "data_structures",
          "그래프 이론": "graphs", "다이나믹 프로그래밍": "dp", "기하학": "geometry"}
     ret = dict()
@@ -67,36 +73,48 @@ async def solvedByTag():
 
 @app.get("/v1/status/level")
 async def statusByLevel():
+    now = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+    print(f">> Log ({now}) : /v1/status/level")
     util.db.commit()
     return util.getStatusByLevel()
 
 
 @app.get("/v1/status/tag")
 async def statusByTag():
+    now = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+    print(f">> Log ({now}) : /v1/status/tag")
     util.db.commit()
     return util.getStatusByTag()
 
 
 @app.get("/v1/unsolved/level")
 async def unsolvedByLevel(level: int):
+    now = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+    print(f">> Log ({now}) : /v1/unsolved/level")
     util.db.commit()
     return util.getUnsolvedByLevel(level)
 
 
 @app.get("/v1/unsolved/tag")
 async def unsolvedByTag(name: str):
+    now = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+    print(f">> Log ({now}) : /v1/unsolved/tag")
     util.db.commit()
     return util.getUnsolvedByTag(name)
 
 
 @app.get("/v1/best/week")
 async def weeklyBest():
+    now = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+    print(f">> Log ({now}) : /v1/best/week")
     util.db.commit()
     return util.getWeeklyBest()
 
 
 @app.get("/v1/best/contrib")
 async def contribBest():
+    now = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+    print(f">> Log ({now}) : /v1/best/contrib")
     util.db.commit()
     return util.getContributeBest()
 
@@ -107,6 +125,8 @@ class Issue(BaseModel):
 
 @app.post("/v1/issue", status_code=status.HTTP_201_CREATED)
 async def issue(item: Issue):
+    now = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+    print(f">> Log ({now}) : /v1/issue")
     with open(f'issues/{str(datetime.datetime.now())}', 'w') as f:
         f.write(item.text)
     return {"statudCode": 200}
