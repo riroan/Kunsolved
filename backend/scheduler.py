1980d732175c7dcccca71986e94f060af4dd6a83
riroan <riroan@naver.com>
2023-06-04 21:30:32 +0900

    chore: 스네이크케이스로 변경

--- backend/scheduler.py
+++ backend/scheduler.py
@@ -3,12 +3,13 @@ import schedule
 import time
 
 util = Utility(True)
-
+print("Scheduling start")
 # schedule.every().monday.at("05:00").do(util.updateAllUserSolved) # 모든 유저 풀이 정보 업데이트 (대회 풀이 목적)
-schedule.every().day.at("05:00").do(util.updateAllUserSolved)
-schedule.every(5).minutes.do(util.addRecentSolved)               # 최근 해결 내역 업데이트
-schedule.every().day.at("03:00").do(util.getProblemInfo)         # 새로운 문제 업데이트
-schedule.every().day.at("02:00").do(util.updateSchoolUser)       # 새로운 유저 업데이트
+schedule.every().day.at("05:00").do(util.update_all_user_solved)
+schedule.every(5).minutes.do(
+    util.add_recent_solved)               # 최근 해결 내역 업데이트
+schedule.every().day.at("03:00").do(util.get_problem_info)         # 새로운 문제 업데이트
+schedule.every().day.at("02:00").do(util.update_school_user)       # 새로운 유저 업데이트
 while True:
     schedule.run_pending()
     time.sleep(1)
