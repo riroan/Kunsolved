dcf1e90504057725fcfd7b72d8d7536ee2584a07
riroan <riroan@naver.com>
2023-06-04 19:28:33 +0900

    feat: 데이터베이스 스펙 변경

--- backend/database.py
+++ backend/database.py
@@ -6,7 +6,8 @@ from sqlalchemy.orm import sessionmaker
 config = yaml.safe_load(open("config.yml"))
 DATABASE_URL = config.get("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_recycle=10000,
+                       pool_size=5, max_overflow=20, echo=False, echo_pool=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
